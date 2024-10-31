ca6dd7bb579d5a60aa9981d24f19b3f4f4ec4046
Cesar-CAAR <cesar.andres5242@gmail.com>
2024-10-31 12:42:03 -0700

    actualizacion

---
+++ Data/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Libreria_CAAR.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Tutilo { get; set; }
+        public string Descripcion { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genero { get; set; }
+        public string Autor { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Libreria_CAAR.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,14 +12,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace Libreria_CAAR
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +34,8 @@ namespace Libreria_CAAR
         {
 
             services.AddControllers();
+            // Configurar DBContext con SQL
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Libreria_CAAR", Version = "v1" });
