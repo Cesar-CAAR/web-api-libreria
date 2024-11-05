2d830d90eb097e5c36e82b2238bab4a85242f916
Cesar-CAAR <cesar.andres5242@gmail.com>
2024-11-05 09:13:11 -0700

    actulizacion 05/11/2024

--- Data/Services/BooksService.cs
+++ Data/Services/BooksService.cs
@@ -1,6 +1,9 @@
 ﻿using Libreria_CAAR.Data.Models;
 using Libreria_CAAR.Data.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Libreria_CAAR.Data.Services
 {
@@ -13,6 +16,9 @@ namespace Libreria_CAAR.Data.Services
             _context = context;
         }
 
+
+
+        // Método que nos permite agregar un nuevo libro en la BD
         public void AddBook(BookVM book)
         {
             var _book = new Books()
@@ -29,6 +35,46 @@ namespace Libreria_CAAR.Data.Services
             };
             _context.Books.Add(_book);
             _context.SaveChanges();
+        }
+
+
+        // Método que nos permite obtener una lista de todos los libros de la BD
+        public List<Books> GetAllBks() => _context.Books.ToList();
+
+
+        // Método que nos permite obtener el libro que estamos pidiendo de la BD
+        public Books GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.Id == bookid);
+
+
+        // Método que nos permite modificar un libro que se encuentra en la BD
+        public Books UpdateBookById(int bookid, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookid);
+            if (_book != null)
+            {
+                _book.Tutilo = book.Tutilo;
+                _book.Descripcion = book.Descripcion;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genero = book.Genero;
+                _book.Autor = book.Autor;
+                _book.CoverUrl = book.CoverUrl;
+
+                _context.SaveChanges();
+            }
+            return _book;
+        }
+
+
+        public void DeleteBookById(int bookid)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookid);
+            if (_book != null)
+            {
+                _context.Books.Remove( _book );
+                _context.SaveChanges();
+            }
         }
     }
 }
